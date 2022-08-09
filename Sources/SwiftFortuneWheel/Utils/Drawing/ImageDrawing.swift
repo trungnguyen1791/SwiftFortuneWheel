de7ff96bf4f85d6d8a6031205804caae4aacbb04
trungnguyen1791 <trungnguyen.1791@gmail.com>
2022-08-09 14:55:24 +0700

    Update ImageDrawing.swift
    Add more slice drawing option

--- Sources/SwiftFortuneWheel/Utils/Drawing/ImageDrawing.swift
+++ Sources/SwiftFortuneWheel/Utils/Drawing/ImageDrawing.swift
@@ -18,7 +18,79 @@ import UIKit
 protocol ImageDrawing {}
 
 extension ImageDrawing {
-    
+    func drawMultipleImages(in context: CGContext, images: [SFWImage], preferences: ImagePreferences, rotations: CGFloat, index: Int, topOffset: CGFloat, radius: CGFloat, margins: SFWConfiguration.Margins) {
+        let flipAngle: CGFloat = 0
+        let step: CGFloat = 10
+        var images = images
+        //There are maximum 4 images.
+        
+        // 1 image  -> shift 0
+        // 2 images -> shif 5
+        // 3 images -> shift 10
+        // 4 images -> shift 15
+        var shift:CGFloat = 0
+        if images.count == 1 {
+            shift = 0
+        }else if images.count == 2 {
+            shift = 5
+        }else if images.count == 3 {
+            shift = 10
+        }else if images.count == 4 {
+            shift = 15
+        }
+        var rotation = rotations - shift
+        
+        //This reversation make the badge image at the end.
+        if preferences.flipUpsideDown {
+            images = images.reversed()
+        }
+        
+        for (idx, item) in images.enumerated() {
+            context.saveGState()
+            context.rotate(by: (flipAngle + rotation) * CGFloat.pi/180)
+            
+            var image = item
+            if let tintColor = preferences.tintColor {
+                image = image.withTintColor(tintColor)
+            }
+            
+            let aspectRatioRect = preferences.preferredSize.aspectFit(sizeImage: image.size)
+            let yPositionWithoutOffset = radius - preferences.verticalOffset - topOffset - margins.top // This value determine image pos in range ( center - outside). Bigger => Center it's
+            
+            let flipYOffset: CGFloat = 0
+            let yPosition = yPositionWithoutOffset + flipYOffset
+            
+            let rectangle = CGRect(x: -(aspectRatioRect.size.width / 2), y: -yPosition, width: aspectRatioRect.size.width, height: aspectRatioRect.size.height)
+            
+            if let backgroundColor = preferences.backgroundColor {
+                backgroundColor.setFill()
+                context.fill(rectangle)
+            }
+            
+            //Rotation is around the origin. To rotate around another point, translate that point to the origin, rotate, and translate back.
+            if preferences.flipUpsideDown {
+                let centerX = rectangle.midX
+                let centerY = rectangle.midY
+                
+                context.translateBy(x: centerX, y: centerY)
+                context.rotate(by: 180 * CGFloat.pi/180)
+                context.translateBy(x: -centerX, y: -centerY)
+            }
+            
+            #if os(macOS)
+            image.draw(in: rectangle)
+            #else
+            image.draw(in: rectangle, blendMode: .normal, alpha: 1)
+            #endif
+            
+            context.restoreGState()
+            
+            rotation += step
+        }
+        
+        
+        
+    }
     /// Draws image
     /// - Parameters:
     ///   - context: context where to draw
